cec524b07b9eb6b7d0047f834a12b64bea9fdcce
Summer Lee <lee@summer.today>
2025-04-08 01:42:52 +0800

    使用 @ant-design/v5-patch-for-react-19 解决 React 19 与 Ant Design v5 的兼容性问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { Suspense } from 'react'
+import { Suspense, version as reactVersion } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import { ConfigProvider, Spin } from 'antd'
+import { ConfigProvider, Spin, version as antdVersion } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import { router } from './routes'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
+// 导入 Ant Design v5 与 React 19 的兼容补丁
+import './utils/antdCompatible'
 import './index.css'
 
 // 设置 dayjs 全局配置
 dayjs.locale('zh-cn')
 dayjs.extend(customParseFormat)
+
+// 打印版本信息，帮助排查兼容性问题
+console.log('React 版本:', reactVersion)
+console.log('Ant Design 版本:', antdVersion)
+console.info('已应用 @ant-design/v5-patch-for-react-19 补丁，解决兼容性问题')
 
 function App() {
   return (
---
+++ src/utils/antdCompatible.ts
@@ -0,0 +1,6 @@
+// 设置 Ant Design 与 React 19 的兼容模式
+// 参考: https://github.com/ant-design/ant-design/issues/44467
+import '@ant-design/v5-patch-for-react-19'
+
+// 注意: 这个补丁包自动应用，不需要额外的调用
+export default {}
